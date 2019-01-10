8d2ed1b176df12ad6a95dc98fffd8add0f5f49e0
ZAHID SCATIZZI <zahid.scatizzi@pucese.edu.ec>
2019-01-09 22:44:20 -0500

    Tarea Multiplicacion Multidimencional

--- AppMultiMatriz/Program.cs
+++ AppMultiMatriz/Program.cs
@@ -8,6 +8,9 @@ namespace AppMultiMatriz
 {
     class Program
     {
+        private int[,] MatrizA;
+        private int[,] MatrizB;
+        private int[,] MatrizC;
         static void Main(string[] args)
         {
             //Matrices Multidimensionales Rectagunlares
@@ -25,6 +28,10 @@ namespace AppMultiMatriz
 
             int[][,] matriz = new int[3][,];
             inicial(matriz);
+
+            Program pv = new Program();
+            pv.CargarMatriz();
+            pv.ImprimirMulti();
 
             Console.ReadKey();
         }
@@ -80,5 +87,58 @@ namespace AppMultiMatriz
             Multi(matriz[0], matriz[1], matriz[2]);
             Imprimir(matriz[2]);
         }
+        public void CargarMatriz()
+        {
+            MatrizA = new int[10, 10];
+            MatrizB = new int[10, 10];
+            MatrizC = new int[10, 10];
+
+            Console.WriteLine("\nMulplicacion Multidimensional \nIngresando datos al matriz A");
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = 1; j <= 3; j++)
+                {
+                    Console.Write("\nIngrese posicion [" + i + "," + j + "]: ");
+                    string linea;
+                    linea = Console.ReadLine();
+                    MatrizA[i, j] = int.Parse(linea);
+                }
+            }
+
+            Console.WriteLine("\nIngresando datos al matriz B");
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = 1; j <= 3; j++)
+                {
+                    Console.Write("\nIngrese posicion [" + i + "," + j + "]: ");
+                    string linea;
+                    linea = Console.ReadLine();
+                    MatrizB[i, j] = int.Parse(linea);
+                }
+            }
+            //Multiplicamos la MatrizA y la MatrizB
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = 1; j <= 3; j++)
+                {
+                    MatrizC[i, j] = MatrizA[i, j] * MatrizB[i, j];
+                }
+            }
+        }
+        public void ImprimirMulti()
+        {
+            Console.WriteLine("\nLa Multiplicacion de la MatrizA y MatrizB es :");
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.Write("\n");
+                for (int j = 1; j <= 3; j++)
+                {
+                    Console.Write(MatrizC[i, j] + "  ");
+                }
+            }
+        }
+
+
     }
 }
+    
